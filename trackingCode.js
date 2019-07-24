ac826c9dfb9153956a6ce455469c861b63e23274
justincersey <justin@cersey.com>
2019-07-24 18:09:43 -0400

    Update trackingCode.js

--- trackingCode.js
+++ trackingCode.js
@@ -1,5 +1,3 @@
-<script>
-	$(function () {
-  		$("#tmp_image-57974 img").attr("src", "https://images.clickfunnels.com/0b/c845e81b86488fba7bc8b943eefe55/CFPDTogImage.png");
-	});
-</script>
+$(function () {
+	$("#tmp_image-57974 img").attr("src", "https://images.clickfunnels.com/0b/c845e81b86488fba7bc8b943eefe55/CFPDTogImage.png");
+});
